a25ed0255c8192dc7581c0c5f6af7c4b3d28cbea
bleh <mwendwablessingm@gmail.com>
2025-02-05 15:32:14 +0300

    new changes

---
+++ Hardware-web-app-frontend/src/LoginPage.js
@@ -0,0 +1,81 @@
+
+import React from "react";
+import { Form, Button } from 'react-bootstrap';
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { login } from "./auth";
+
+const LoginPage = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const navigate = useNavigate();
+
+    const loginUser = (data) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+
+        fetch('/auth/login', requestOptions)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Login failed');
+                }
+                return res.json();
+            })
+            .then((data) => {
+                if (data.access_token) {
+                    login(data);
+                    navigate('/'); 
+                } else {
+                    console.error('Access token missing in response');
+                }
+            })
+            .catch((error) => console.error('Login error:', error)); 
+
+        reset();
+    };
+
+    return (
+        <div className="container">
+            <div className="form">
+                <h1>Login Page</h1>
+                <form>
+                    <Form.Group>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" placeholder="Your username"
+                            {...register('username', { required: true, maxLength: 25 })}
+                        />
+                    </Form.Group>
+                    {errors.username && <p style={{ color: 'red' }}><small>Username is required</small></p>}
+                    {errors.username?.type === "maxLength" && <p style={{ color: 'red' }}><small>Username should be 25 characters</small></p>}
+                    <br />
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Your password"
+                            {...register('password', { required: true, minLength: 8 })}
+                        />
+                    </Form.Group>
+                    {errors.password && <p style={{ color: 'red' }}><small>Password is required</small></p>}
+                    {errors.password?.type === "minLength" && <p style={{ color: 'red' }}><small>Password should be more than 8 characters</small></p>}
+                    <br />
+                    <Form.Group>
+                        <Button variant="primary" type="submit" onClick={handleSubmit(loginUser)}>
+                            Login
+                        </Button>
+                    </Form.Group>
+                    <br />
+                    <Form.Group>
+                        <small>
+                            Do not have an account? <Link to="/signup">Sign Up</Link>
+                        </small>
+                    </Form.Group>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ Hardware-web-app-frontend/src/SignUpPage.js
@@ -0,0 +1,132 @@
+
+import React, {useState} from "react";
+import {Form,Button, Alert} from 'react-bootstrap'
+import { Link } from "react-router-dom"
+import {useForm} from 'react-hook-form'
+
+
+const SignUpPage =()=>{
+
+    const {register, handleSubmit,reset, formState: {errors}} = useForm();
+    const [show, setShow] = useState(false);
+
+    const [serverResponse, setServerResponse] = useState('')
+
+    const submitForm = (data) => {
+
+      if (data.password === data.confirmPassword){
+
+        const body={
+            username: data.username,
+            email: data.email,
+            password: data.password
+        }
+
+      const requestOPtions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      }
+
+      fetch('/auth/signup', requestOPtions)
+       .then(response => response.json())
+       .then(data => {
+        setServerResponse(data.message)
+        console.log(serverResponse)
+          console.log(data);
+          //alert("Registration Successful")
+
+          setShow(true);
+        })
+        .catch(error => console.error('Error:', error));
+
+      reset()
+
+    }
+    else{
+      alert("Passwords do not match")
+    }
+
+}
+    
+  
+
+    return (
+        <div className="container">
+            <div className="form">
+               
+          {show?
+          <>
+          <Alert variant="success" onClose={() => setShow(false)} dismissible>
+                
+                <p>
+               {serverResponse}
+                </p>
+              </Alert>
+           <h1>Sign Up Page</h1>
+                
+              </>
+              :
+              <h1>Sign Up Page</h1>
+
+          }
+                <form onSubmit={handleSubmit(submitForm)}>
+                     <Form.Group>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" placeholder="Your username"
+                        {...register("username",{required:true, maxLength:100})} 
+                        />
+                        
+                        {errors.username && <p style={{color:"red"}}><small>Username is required</small></p>}
+
+                     </Form.Group>
+                     <br></br>
+                     <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Your email"
+                        {...register("email",{required:true, maxLength:100})}
+                        />
+                        
+                        {errors.email && <p style={{color:"red"}}><small>Email is required</small></p>}
+                     </Form.Group>
+                     <br></br>
+                     <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Create pasword" 
+                        {...register("password",{required:true, minLength:8})}
+                         
+                         />
+                         
+                         {errors.password && <p style={{color:"red"}}><small>Password is required and must be at least 8 characters long</small></p>}
+                     </Form.Group>
+                     <br></br>
+                     <Form.Group>
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" placeholder="Your password"
+                         {...register("confirmPassword",{required:true, minLength:8})}
+                        />
+                         
+                         {errors.confirmPassword && <p style={{color:"red"}}><small>Passwords do not match</small></p>}
+                     </Form.Group>
+                     <br></br>
+                     <Form.Group>
+                        <Button variant="primary" type="submit" onClick={handleSubmit(submitForm)}>
+                            Sign Up
+                        </Button>
+                     </Form.Group> 
+                     <br></br>
+                     <Form.Group>
+                            <small>
+                              Already have an account? <Link to="/login">Login</Link>
+                            </small> 
+                         </Form.Group>
+                </form>
+            </div>
+        </div>
+    )
+
+}
+
+export default SignUpPage;
