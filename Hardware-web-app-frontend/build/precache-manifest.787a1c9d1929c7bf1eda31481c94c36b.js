d2310e64f0f2d5726bc59e3b3c7dd41cab27235e
bleh <mwendwablessingm@gmail.com>
2025-02-06 14:54:47 +0300

    deploy

---
+++ Hardware-web-app-frontend/build/precache-manifest.787a1c9d1929c7bf1eda31481c94c36b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a0da7687f428c6786c64658cd9cea6",
+    "url": "/ble-0/HARDWARE-WEB-APPindex.html"
+  },
+  {
+    "revision": "9e0e31fd5d771ba1535a",
+    "url": "/ble-0/HARDWARE-WEB-APPstatic/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "56351771eb6dacb4a820",
+    "url": "/ble-0/HARDWARE-WEB-APPstatic/css/main.0a2d0e09.chunk.css"
+  },
+  {
+    "revision": "9e0e31fd5d771ba1535a",
+    "url": "/ble-0/HARDWARE-WEB-APPstatic/js/2.52530382.chunk.js"
+  },
+  {
+    "revision": "f77de26868143a9112750cd92c9beceb",
+    "url": "/ble-0/HARDWARE-WEB-APPstatic/js/2.52530382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56351771eb6dacb4a820",
+    "url": "/ble-0/HARDWARE-WEB-APPstatic/js/main.21a0b2b1.chunk.js"
+  },
+  {
+    "revision": "ec9d026b21db84659d56",
+    "url": "/ble-0/HARDWARE-WEB-APPstatic/js/runtime-main.72e94206.js"
+  }
+]);
